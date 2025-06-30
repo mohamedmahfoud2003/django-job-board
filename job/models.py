1c69f0df768fcb68679c930e48af3cb0d2c0767a
mahfoud <benoumhanimahfoud2003@gmail.com>
2025-06-30 01:55:24 -0700

    add job views and urls

--- job/models.py
+++ job/models.py
@@ -22,10 +22,18 @@ class Job(models.Model): # tabel
     published_date = models.DateTimeField(auto_now=True)
     vacancy = models.IntegerField(default=1)
     salary =  models.IntegerField(default=0)
-    # -category
     experience = models.IntegerField(default=1)
-
-
+    category = models.ForeignKey('Category',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+    
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
+
+
+    
